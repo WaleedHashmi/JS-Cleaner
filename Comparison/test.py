8ab136f1abcb3dae8ad76902c340363d3edeac19
Waleed Hashmi <waleedahmedhashmi@gmail.com>
2019-06-15 14:23:26 +0400

    .

--- Comparison/test.py
+++ Comparison/test.py
@@ -2,20 +2,26 @@ import requests, csv
 from bs4 import BeautifulSoup as bs
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+
 
 
 ##############################
 ### Extracting Source code ###
 ##############################
-URL = 'https://en.wikipedia.org/wiki/Multi-armed_bandit'
-res=requests.get(URL)
-soup=bs(res.text,"lxml")
+URL = 'https://www.faa.gov'
+
+options = Options()
+profile = webdriver.FirefoxProfile('/Users/waleed/Library/Application Support/Firefox/Profiles/dmopb59z.default-release')
+driver = webdriver.Firefox(profile, options=options, executable_path="/Users/waleed/Desktop/JS-Reseach/Comparison/driver/geckodriver")
+driver.get(URL)
+
+soup = bs(driver.page_source.encode("utf-8"),"lxml")
 
 output = open('unedited.txt', mode='w')
 # output = csv.writer(restaurant_output, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
 output.write(str(soup))
-
 
 IDs = []
 classes = []
@@ -52,11 +58,19 @@ for tag in ["div","p","span","h1","h2","h3","h4","h5","h6","p","button"]:
 for a in (IDs + classes):
     print (a)
 
-
 ########################
 ### Selenium Scoring ###
 ########################
+i=0
+for id in IDs:
+    try:
+        elem = driver.find_element_by_id(id[1])
+        actions = ActionChains(driver)
+        driver.execute_script("arguments[0].scrollIntoView();", elem)
+        actions.move_to_element(elem)
+        actions.perform()
+    except:
+        i+=1
 
-options = Options()
-driver = webdriver.Firefox(options=options, executable_path="/Users/waleed/Desktop/JS-Reseach/Comparison/driver/geckodriver")
-driver.get(URL)
+
+print (i,len(IDs))
