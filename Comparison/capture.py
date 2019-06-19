22e93af8eca93d25e9a274fdbf6602cc6946a464
Waleed Hashmi <waleedahmedhashmi@gmail.com>
2019-06-19 11:08:13 +0400

    .

--- Comparison/capture.py
+++ Comparison/capture.py
@@ -40,6 +40,9 @@ for link in URLs:
         IDs = []
         classes = []
 
+        ids_classes = [] #list of both classes and ids
+
+
         for tag in ["nav","li","ul","div","p","span","h1","h2","h3","h4","h5","h6","p","button"]:
             tags = soup.findAll (tag)
             for t in tags:
@@ -50,6 +53,7 @@ for link in URLs:
                     if id != None and (not id in IDs) and (type(id)==type('str') or (type(id)==type(['list']) and len(id)==1)):
                         if type(id) == type([]):id = id[0]
                         IDs.append([tag,id])
+                        ids_classes.append([tag,'id',id])
                 except:
                     pass
                 # storing all div's classes
@@ -59,16 +63,76 @@ for link in URLs:
                     if cl != None and (not cl in classes) and (type(cl)==type('str') or (type(cl)==type(['list']) and len(cl)==1)) :
                         if type(cl) == type([]):cl = cl[0]
                         classes.append([tag,cl])
+                        ids_classes.append([tag,'class',cl])
                 except:
                     pass
 
         #######################################
         ### 2. Find elements and Screenshot ###
         #######################################
-        for id in IDs:
+        # for id in IDs:
+        #     try:
+        #         # find element in the code
+        #         elem = driver.find_element_by_id(id[1])
+        #
+        #         # get y position and scroll to that location
+        #         location = elem.location.get('y')
+        #         scrollScript = "setTimeout(function(){window.scrollTo(0," + str(location) + ");}, 2000);"
+        #         driver.execute_script(scrollScript)
+        #         time.sleep(.5)
+        #
+        #         # hover over that element
+        #         actions = ActionChains(driver)
+        #         driver.execute_script("arguments[0].scrollIntoView();", elem)
+        #         actions.move_to_element(elem)
+        #         actions.perform()
+        #
+        #         # take a screenshot and store it
+        #         if not os.path.exists("ss/" + folderName):
+        #             os.mkdir("ss/" + folderName)
+        #             os.mkdir("ss/" + folderName + "/0")
+        #             os.mkdir("ss/" + folderName + "/1")
+        #             os.mkdir("ss/" + folderName + "/2")
+        #         ssPath = "ss/" + folderName + "/" + str(varients.index(proxyVarient)) + "/screenshot{}.png".format(id[1])
+        #         driver.save_screenshot(ssPath)
+        #         print ("Capturing Screenshot for ID =", id[1])
+        #         time.sleep(.5)
+        #     except:
+        #         print ("Error parsing ID =", id[1])
+        # for cl in classes:
+        #     try:
+        #         # find element in the code
+        #         elem = driver.find_element_by_class_name(cl[1])
+        #
+        #         # get y position and scroll to that location
+        #         location = elem.location.get('y')
+        #         scrollScript = "setTimeout(function(){window.scrollTo(0," + str(location) + ");}, 2000);"
+        #         driver.execute_script(scrollScript)
+        #         time.sleep(.5)
+        #
+        #         # hover over that element
+        #         actions = ActionChains(driver)
+        #         driver.execute_script("arguments[0].scrollIntoView();", elem)
+        #         actions.move_to_element(elem)
+        #         actions.perform()
+        #
+        #         # take a screenshot and store it
+        #         if not os.path.exists("ss/" + folderName):
+        #             os.mkdir("ss/" + folderName)
+        #             os.mkdir("ss/" + folderName + "/0")
+        #             os.mkdir("ss/" + folderName + "/1")
+        #             os.mkdir("ss/" + folderName + "/2")
+        #         ssPath = "ss/" + folderName + "/" + str(varients.index(proxyVarient)) + "/screenshot{}.png".format(id[1])
+        #         driver.save_screenshot(ssPath)
+        #         print ("Capturing Screenshot for Class =", cl[1])
+        #         time.sleep(.5)
+        #     except:
+        #         print ("Error parsing Class =", cl[1])
+
+        for idclass in ids_classes:
             try:
                 # find element in the code
-                elem = driver.find_element_by_id(id[1])
+                elem = driver.find_element_by_xpath("//{}[@{}='{}']".format(idclass[0],idclass[1],idclass[2]))
 
                 # get y position and scroll to that location
                 location = elem.location.get('y')
@@ -88,42 +152,11 @@ for link in URLs:
                     os.mkdir("ss/" + folderName + "/0")
                     os.mkdir("ss/" + folderName + "/1")
                     os.mkdir("ss/" + folderName + "/2")
-                ssPath = "ss/" + folderName + "/" + str(varients.index(proxyVarient)) + "/screenshot{}.png".format(id[1])
+                ssPath = "ss/" + folderName + "/" + str(varients.index(proxyVarient)) + "/screenshot{}-{}-{}.png".format(idclass[0],idclass[1],idclass[2])
                 driver.save_screenshot(ssPath)
-                print ("Capturing Screenshot for ID =", id[1])
+                print ("Capturing Screenshot of {} = {}".format(idclass[1],idclass[2]))
                 time.sleep(.5)
             except:
-                print ("Error parsing ID =", id[1])
-
-
-        for cl in classes:
-            try:
-                # find element in the code
-                elem = driver.find_element_by_class_name(cl[1])
-
-                # get y position and scroll to that location
-                location = elem.location.get('y')
-                scrollScript = "setTimeout(function(){window.scrollTo(0," + str(location) + ");}, 2000);"
-                driver.execute_script(scrollScript)
-                time.sleep(.5)
-
-                # hover over that element
-                actions = ActionChains(driver)
-                driver.execute_script("arguments[0].scrollIntoView();", elem)
-                actions.move_to_element(elem)
-                actions.perform()
-
-                # take a screenshot and store it
-                if not os.path.exists("ss/" + folderName):
-                    os.mkdir("ss/" + folderName)
-                    os.mkdir("ss/" + folderName + "/0")
-                    os.mkdir("ss/" + folderName + "/1")
-                    os.mkdir("ss/" + folderName + "/2")
-                ssPath = "ss/" + folderName + "/" + str(varients.index(proxyVarient)) + "/screenshot{}.png".format(id[1])
-                driver.save_screenshot(ssPath)
-                print ("Capturing Screenshot for Class =", cl[1])
-                time.sleep(.5)
-            except:
-                print ("Error parsing Class =", cl[1])
+                print ("Error parsing of {} = {}".format(idclass[1],idclass[2]))
 
         driver.close()
